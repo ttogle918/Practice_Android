e555c5b1954d6c9b2ebf4a8d7333b44517b01524
hyun <ttogle918@gmail.com>
2020-12-23 18:03:15 +0900

    use Drawer Layout

--- app/src/main/java/com/androidapp/practiceapp/MainActivity.java
+++ app/src/main/java/com/androidapp/practiceapp/MainActivity.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button buttonOpen, buttonClose;
     CheckBox checkboxLock;
     DrawerLayout drawer;
-
+    ImageView viewLeftOpen, viewRightOpen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         init();
     }
+
     @Override
     public void onClick(View v) {
         if (!checkboxLock.isChecked()) {
@@ -37,21 +41,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
             }
         }
-/*
-        switch ( v.getId() ) {
-            case R.id.lock:
-                if (checkboxLock.isChecked()) {
-                    if (drawer.isDrawerOpen(Gravity.LEFT)) {
-                        drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
-                    } else {
-                        drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
-                    }
-                } else {
-                    drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-                }
-                break;
-        }
-        */
     }
 
     public void init() {
@@ -62,5 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonOpen.setOnClickListener(this);
         buttonClose = (Button) findViewById(R.id.btn_close) ;
         buttonClose.setOnClickListener(this);
+        viewLeftOpen = (ImageView) findViewById(R.id.v_left);
+        viewRightOpen = (ImageView) findViewById(R.id.v_right);
     }
 }
